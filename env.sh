23ca0c3c4ecd7f95b94c0d6c07d7f253d3c2b269
whzruc <whzsxdt@126.com>
2024-10-12 13:09:11 +0800

    fix mini pixels path

--- env.sh
+++ env.sh
@@ -1,2 +1,10 @@
+#!/bin/zsh
+
+# Export current directory to PIXELS_SRC and PIXELS_HOME
 export PIXELS_SRC=$(pwd)
 export PIXELS_HOME=$(pwd)
+
+# Optionally print the values to verify
+echo "PIXELS_SRC is set to $PIXELS_SRC"
+echo "PIXELS_HOME is set to $PIXELS_HOME"
+
