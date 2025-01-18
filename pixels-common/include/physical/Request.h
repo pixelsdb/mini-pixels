90be7ec6bf71a71d0a34a701b3bf6ba815495633
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2025-01-18 19:41:30 +0800

    fix: add missing includes for GCC 14 compatibility
    This commit adds the necessary #include directives to resolve compilation errors encountered with GCC 14.

--- pixels-common/include/physical/Request.h
+++ pixels-common/include/physical/Request.h
@@ -5,16 +5,18 @@
 #ifndef PIXELS_REQUEST_H
 #define PIXELS_REQUEST_H
 
+#include <cstdint>
 #include <iostream>
 
 class Request {
-public:
+  public:
     int64_t bufferId;
     uint64_t queryId;
     uint64_t start;
     uint64_t length;
-    Request(uint64_t queryId_, uint64_t start_, uint64_t length_, int64_t bufferId = -1);
+    Request(uint64_t queryId_, uint64_t start_, uint64_t length_,
+            int64_t bufferId = -1);
     int hashCode();
     int comparedTo(Request o);
 };
-#endif //PIXELS_REQUEST_H
+#endif // PIXELS_REQUEST_H
